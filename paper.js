cdca1eadbe1bdaa3c989c3aa5fea238ea956d6d7
LyenaDsouza <72792975+LyenaDsouza@users.noreply.github.com>
2021-01-28 15:29:16 +0530

    Add files via upload

---
+++ paper.js
@@ -0,0 +1,30 @@
+class Paper
+{
+    constructor(x,y,r)
+    {
+        var options={
+            isStatic:false,
+            restitution:0.3,
+            friction:0.5,
+            density:1.2
+        }
+      this.body=Bodies.circle(x,y,r,options) 
+      this.x=x;
+      this.r=r;
+      this.y=y;
+      this.paper=loadImage("paper.png");
+      World.add(world,this.body);
+
+    }
+    display()
+    {
+        
+        var paper1=this.body.position;
+        push();
+        translate(paper1.x,paper1.y);
+        //fill("yellow");
+        
+        image(this.paper,0,0,this.r,this.r);
+       pop();
+    }
+}
